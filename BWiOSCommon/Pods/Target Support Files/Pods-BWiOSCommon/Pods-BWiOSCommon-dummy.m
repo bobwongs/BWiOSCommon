c416dd43d690d4513bc34e3569fa2c4575a1e9b6
wangbo <wangbo@bluemoon.com.cn>
2016-12-02 10:21:33 +0800

    modify

---
+++ BWiOSCommon/Pods/Target Support Files/Pods-BWiOSCommon/Pods-BWiOSCommon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BWiOSCommon : NSObject
+@end
+@implementation PodsDummy_Pods_BWiOSCommon
+@end
---
+++ BWiOSCommon/Pods/Target Support Files/Pods-BWiOSCommonTests/Pods-BWiOSCommonTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BWiOSCommonTests : NSObject
+@end
+@implementation PodsDummy_Pods_BWiOSCommonTests
+@end
