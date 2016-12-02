430b1c58f5e61bdbebf658d9c157ffd07764dc5d
wangbo <wangbo@bluemoon.com.cn>
2016-12-02 11:03:51 +0800

    Modify

---
+++ BWiOSCommon/BWiOSCommon/Common/Utility/BWUtility.h
@@ -0,0 +1,13 @@
+//
+//  BWUtility.h
+//  BWiOSCommon
+//
+//  Created by BobWong on 16/12/2.
+//  Copyright © 2016年 BobWongStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BWUtility : NSObject
+
+@end
---
+++ BWiOSCommon/BWiOSCommon/Common/Utility/BWUtility.m
@@ -0,0 +1,13 @@
+//
+//  BWUtility.m
+//  BWiOSCommon
+//
+//  Created by BobWong on 16/12/2.
+//  Copyright © 2016年 BobWongStudio. All rights reserved.
+//
+
+#import "BWUtility.h"
+
+@implementation BWUtility
+
+@end
